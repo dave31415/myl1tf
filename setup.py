1e25d3164c207d413a37738c11357e25ee103ab1
David Johnston <dave31415@gmail.com>
2014-11-24 16:19:37 -0500

    added license and setup.py and edited Readme

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='myl1tf',
+      version='0.1',
+      description='L1 trend filtering with seasonality',
+      author='David Johnston',
+      author_email='dave31415@gmail.com',
+      url='',
+      packages=['myl1tf'],
+      install_requires=[]
+     )
